e1902ab866d394414bb766276afedc587aa28e45
sagor devnath <ssagor64@gmail.com>
2022-10-19 12:23:09 +0600

    update mose popular section

--- src/components/mostPopular/MostPopular.js
+++ src/components/mostPopular/MostPopular.js
@@ -2,24 +2,24 @@ import React from 'react'
 
 export default function MostPopular() {
   return (
-    <div className='h-100 mb-3 bg-white'>
+    <div className='h-100 mb-3 bg-blue-400'>
     <div className="text-2xl mx-6">MostPopular</div>
     <hr className='mx-6' />
-    <div class="card h-40 bg-base-100 shadow-xl image-full">
+    <div class="card h-40 bg-base-100 shadow-xl image-full border-double border-4 border-sky-500 rounded-none">
   <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
   <div class="card-body">
     <h2 class="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
   </div>
 </div>
-<div class="card h-40 bg-base-100 shadow-xl image-full">
+<div class="card h-40 bg-base-100 shadow-xl image-full border-double border-4 border-sky-500 rounded-none">
   <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
   <div class="card-body">
     <h2 class="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
   </div>
 </div>
-<div class="card h-40 bg-base-100 shadow-xl image-full">
+<div class="card h-40 bg-base-100 shadow-xl image-full border-double border-4 border-sky-500 rounded-none">
   <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
   <div class="card-body">
     <h2 class="card-title">Shoes!</h2>
