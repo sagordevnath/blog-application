2f5ecd0a0605eea10b47bac3d5bbd57cf9954d09
sagor devnath <ssagor64@gmail.com>
2022-10-19 12:06:39 +0600

    most popular section structured

--- src/components/mostPopular/MostPopular.js
+++ src/components/mostPopular/MostPopular.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function MostPopular() {
   return (
-    <div className="h-6 bg-blue-400 mb-3 bg-yellow-400">MostPopular</div>
+    <div className='h-100 mb-3 bg-white'>
+    <div className="text-2xl mx-6">MostPopular</div>
+    <hr className='mx-6' />
+    <div class="card h-40 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+  </div>
+</div>
+<div class="card h-40 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+  </div>
+</div>
+<div class="card h-40 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+  </div>
+</div>
+    </div>
   )
 }
