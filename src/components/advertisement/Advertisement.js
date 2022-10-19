e4dddde2111f570fde9f2adf32d2e535f2971d09
sagor devnath <ssagor64@gmail.com>
2022-10-19 11:52:44 +0600

    advertisement section structured

--- src/components/advertisement/Advertisement.js
+++ src/components/advertisement/Advertisement.js
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Advertisement() {
   return (
-    <div className='h-6 mb-3 bg-red-400'>Advertisement</div>
+    <div className='h-100 mb-3 bg-white'>
+    <div className='text-2xl mx-6'>Advertisement</div>
+    <hr className='mx-6' />
+    <div className='m-6'>
+    <div className="h-auto">
+    <div class="hero" >
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <p>Try this theme</p>
+      <h1 class="mb-5 text-3xl font-bold">For Free</h1>
+      <hr className="w-8 mx-auto" />
+      <p class="mb-5">Provident cupiditate voluptatem et in repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Try it for free</button>
+    </div>
+  </div>
+</div>
+    </div>
+    </div>
+    </div>
   )
 }
